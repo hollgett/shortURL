32da465137eaad2f135e1e8a50f5c9b18fe832a5
hollget <danikm4a1@gmail.com>
2024-11-11 18:57:21 +0500

    add function main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func shortUrlPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, `http://localhost:8080/EwHXdJfB `)
+}
+
+func shortUrlGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Location", "https://practicum.yandex.ru/")
+	w.WriteHeader(http.StatusTemporaryRedirect)
+
+}
+
+func main() {
+	rtr := http.NewServeMux()
+	rtr.HandleFunc(`/`, shortUrlPost)
+	rtr.HandleFunc(`/EwHXdJfB`, shortUrlGet)
+	http.ListenAndServe(`:8080`, rtr)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hollgett/shortURL.git
+
+go 1.20
