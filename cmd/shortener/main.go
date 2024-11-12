b39652123b48a87cc20980353cd7e4576e1b2dc1
hollget <danikm4a1@gmail.com>
2024-11-12 20:42:02 +0500

    refactoring code in main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -9,53 +9,71 @@ import (
 
 var routes = map[string]http.HandlerFunc{}
 
-func shortURLPost(w http.ResponseWriter, r *http.Request) {
-	// checking for method get
-	if r.Method == http.MethodGet {
-		if handler, ok := routes[r.URL.Path]; ok {
-			// starting handler if exists
-			handler(w, r)
+func randomID() string {
+	//create random URL path
+	shortLink := make([]byte, 8)
+	for i := range shortLink {
+		if rand.Intn(2) == 0 {
+			shortLink[i] = byte(rand.Intn(26) + 65)
 		} else {
-			w.WriteHeader(http.StatusBadRequest)
-			return
+			shortLink[i] = byte(rand.Intn(26) + 97)
 		}
 	}
-	//checking for method post 
-	if r.Method == http.MethodPost {
-		r.Header.Set("Content-Type", "text/plain")
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+	return string(shortLink)
+}
+
+func shortURLPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	r.Header.Set("Content-Type", "text/plain")
+	body, err := io.ReadAll(r.Body)
+	if err != nil || len(body) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	//create short route
+	link := randomID()
+	//handler body short URL
+	routes[link] = func(w http.ResponseWriter, r *http.Request) {
+		stockURL := string(body)
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
-		//create random URL path
-		shortLink := make([]byte, 8)
-		for i := range shortLink {
-			if rand.Intn(2) == 0 {
-				shortLink[i] = byte(rand.Intn(26) + 65)
-			} else {
-				shortLink[i] = byte(rand.Intn(26) + 97)
-			}
-		}
-		//create short route   
-		link := `/` + string(shortLink)
-		routes[link] = func(w http.ResponseWriter, r *http.Request) {
-			stockURL := string(body)
-			if r.Method != http.MethodGet {
-				w.WriteHeader(http.StatusMethodNotAllowed)
-				return
-			}
-			w.Header().Set("Location", stockURL)
-			w.WriteHeader(http.StatusTemporaryRedirect)
-		}
-		w.WriteHeader(http.StatusCreated)
-		fmt.Fprint(w, `http://localhost:8080`+link)
+		w.Header().Set("Location", stockURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, `http://localhost:8080/%s`, link)
+}
+
+func shortURLGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	if handler, ok := routes[r.URL.Path[1:]]; ok {
+		// starting handler if exists
+		handler(w, r)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 }
 
 func main() {
 	rtr := http.NewServeMux()
-	rtr.HandleFunc(`/`, shortURLPost)
+	rtr.HandleFunc(`/`, func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			shortURLPost(w, r)
+		} else {
+			shortURLGet(w,r)
+		}
+	})
 	if err := http.ListenAndServe(`:8080`, rtr); err != nil {
 		panic(err)
 	}
