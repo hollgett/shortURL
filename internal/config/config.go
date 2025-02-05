4959c824e85be1ed3fc6529e0dcad84c9685cc24
hollget <danikm4a1@gmail.com>
2025-02-05 11:46:13 +0500

    test

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,7 +16,7 @@ var Config struct {
 func InitConfig() error {
 	flag.StringVar(&Config.Addr, "a", ":8080", "host and port to run server :port")
 	flag.StringVar(&Config.BaseURL, "b", "http://localhost:8080", "static short url")
-	flag.StringVar(&Config.FileStorage, "f", "tmp/short-url-db.json", "storage data")
+	flag.StringVar(&Config.FileStorage, "f", "", "storage data")
 	flag.StringVar(&Config.DataBase, "d", "", "name data base")
 	flag.Parse()
 	if err := env.Parse(&Config); err != nil {
